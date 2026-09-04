1f98fbdc03816b2752e7e245e1792ec5189afd5b
user1 <user1@MBP-von-user1.fritz.box>
2026-09-05 01:49:45 +0200

    Update format

---
+++ run-localy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# SCRIPT: run-localy.sh 
+# AUTHOR: ...
+# DATE: 2026-09-05T00:31:19
+# REV: 1.0
+# ARGUMENTS: [1:        ][2:		][3:		][4:        ]
+#
+# PURPOSE: run using mkdoc serve 
+#
+# set -x # Uncomment to debug
+# set -n # Uncomment to check script syntax without execution
+# set -e # Break on the first failure
+
+
+mkdocs serve
